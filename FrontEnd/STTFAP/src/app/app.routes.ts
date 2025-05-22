9ffcd6ca482860dd57877ad5fcc4d00e13e6bd85
BrunodeMatos01 <brunodematos3@gmail.com>
2025-05-22 07:24:03 -0300

    Pagina-Login

--- FrontEnd/STTFAP/src/app/app.routes.ts
+++ FrontEnd/STTFAP/src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { MenuComponent } from './menu/menu.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
-  { path: 'login', component: LoginComponent, pathMatch: 'full' }
+  { path: 'login', component: LoginComponent, pathMatch: 'full' },
+  {path: 'menu', component: MenuComponent}
 ];
--- FrontEnd/STTFAP/src/app/login/login.component.ts
+++ FrontEnd/STTFAP/src/app/login/login.component.ts
@@ -1,11 +1,35 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  email: string = '';
+  senha: string = '';
 
+  constructor(private http: HttpClient) {}
+
+  onSubmit() {
+    const dadosLogin = {
+      email: this.email,
+      senha: this.senha
+    };
+
+    this.http.post('http://localhost:3000/api/login', dadosLogin)
+      .subscribe({
+        next: (res) => {
+          console.log('Login realizado com sucesso!', res);
+        },
+        error: (err) => {
+          console.error('Erro ao fazer login:', err);
+        }
+      });
+  }
 }
